4fa27d73a618242e7ce0ac8194dd507e4376fc23
Matheus da Silva Rocha <101686026+Rumplir@users.noreply.github.com>
2025-07-02 18:29:39 -0300

    Initial commit

---
+++ csharp2/Entities/Order.cs
@@ -0,0 +1,61 @@
+﻿using S09EP05.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace S09EP05.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item2 in Items)
+            {
+                sum += item2.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Order moment: {Moment}");
+            sb.AppendLine($"Order status: {Status}");
+            sb.AppendLine($"Client: {Client}");
+            sb.AppendLine("Order Items: ");
+            foreach (OrderItem item in Items)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            sb.AppendLine($"Total Price: {Total():C2}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ csharp3/Program.cs
@@ -0,0 +1,49 @@
+﻿using S10EP01.Entities;
+using System.Globalization;
+
+namespace S10EP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employee: ");
+            int qntEmployee = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= qntEmployee; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                string yn = Console.ReadLine();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per Hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (yn == "y" || yn == "Y")
+                {
+                    Console.Write("Additional charge: ");
+                    double additional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    list.Add(new OutsoucerdEmployee(name, hours, valuePerHour, additional));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.ToString());
+            }
+
+        }
+    }
+}
